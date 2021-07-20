49b3aa39897c5a8bbae03bced0083d700027dd21
Lokesh Poovaragan <dude.1996@live.com>
2021-07-20 16:38:56 +0530

    fix fn names, add comments

--- examples/opencv/edges/edges.js
+++ examples/opencv/edges/edges.js
@@ -1,10 +1,18 @@
 const FileHound = require('filehound');
 const cv = require('opencv');
 
-const eachFileProcessor = (eachFile) => {
+// makeEdges reads an image from /pfs/images and outputs the result of running
+// edge detection on that image to /pfs/out. Note that /pfs/images and
+// /pfs/out are special directories that Pachyderm injects into the container.
+
+const makeEdges = (eachFile) => {
   eachFile.forEach((oneFile) => {
     console.log('Processing', oneFile);
+    
+    // Collect the filename, without the extension
     const tail = oneFile.split('/')[3].split('.')[0];
+
+    // Read an img file and convert it into its edge detected equivalent
     cv.readImage(oneFile, function (err, im) {
       im.convertGrayscale();
       im.gaussianBlur([3, 3]);
@@ -14,8 +22,11 @@ const eachFileProcessor = (eachFile) => {
   });
 };
 
+// Collect all the files that end with .jpg in the /pfs/images folder to be 
+// processed in the edges function
+
 const files = FileHound.create().paths('/pfs/images').ext('jpg').find();
 
 files.then((forEachFile) => {
-  eachFileProcessor(forEachFile);
+  makeEdges(forEachFile);
 });
