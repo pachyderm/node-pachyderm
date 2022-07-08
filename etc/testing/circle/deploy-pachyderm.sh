77e2edbbf85592195f9c965aa6e24de27ca50a99
work <peterjfranchina@gmail.com>
2022-07-07 20:23:58 -0400

    fix loki check

--- etc/testing/circle/deploy-pachyderm.sh
+++ etc/testing/circle/deploy-pachyderm.sh
@@ -7,8 +7,13 @@ export PACHYDERM_VERSION="$(jq -r .pachyderm version.json)"
 
 helm repo add pachyderm https://pachyderm.github.io/helmchart
 helm repo update
-helm install pachd pachyderm/pachyderm --set deployTarget=LOCAL --version ${PACHYDERM_VERSION}
+helm install pachd pachyderm/pachyderm --set deployTarget=LOCAL --version ${PACHYDERM_VERSION} --set console.enabled=false
 
+# kubectl wait --for=condition=available deployment -l app=pachd --timeout=5m
+# kubectl wait statefulset.apps/pachd-loki --for=jsonpath='{.status.readyReplicas}'=1 --timeout=5m
+# There is a bug with wait for json path https://github.com/kubernetes/kubectl/issues/1236
+
+sleep 60s
 kubectl wait --for=condition=available deployment -l app=pachd --timeout=5m
-kubectl wait statefulset.apps/pachd-loki --for=jsonpath='{.status.readyReplicas}'=1 --timeout=5m
+
 pachctl version
